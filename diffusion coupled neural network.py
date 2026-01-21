79800244ace82ddb4a8b3101046610fff7cac0e7
xanni33 <ania_gruca@interia.pl>
2026-01-21 19:02:12 +0100

    add code :p

---
+++ diffusion coupled neural network.py
@@ -0,0 +1,47 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+#parametry modelu
+a = 0.7
+b = 0.8
+epsilon = 0.08
+I = 0.5
+D = 1.0 #WSPOLCZYNNIK DYFUZJI
+
+#parametry siatki
+Nx, Ny = 50, 50
+dx = 1.0
+dt = 0.01
+steps = 3000
+
+v = -1 * np.ones((Nx, Ny))
+w =  1 * np.ones((Nx, Ny))
+
+#lokalne xrodlo impulsu
+v[Nx//2, Ny//2] = 1.0
+
+def laplacian(Z):
+    return (
+        np.roll(Z, 1, axis=0) +
+        np.roll(Z, -1, axis=0) +
+        np.roll(Z, 1, axis=1) +
+        np.roll(Z, -1, axis=1) -
+        4 * Z
+    ) / dx**2
+
+for step in range(steps):
+    dv = v - (v**3)/3 - w + I + D * laplacian(v)
+    dw = epsilon * (v + a - b*w)
+
+    v += dt * dv
+    w += dt * dw
+
+    if step % 500 == 0:
+        plt.figure(figsize=(4, 4))
+        plt.imshow(v, cmap="viridis")
+        plt.colorbar(label="v")
+        plt.title(f"aktywność sieci – krok {step}")
+        plt.axis("off")
+        plt.show()
+
+
---
+++ singular neuron.py
@@ -0,0 +1,44 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+#parametry modelu fitzhugh–nagumo 0.7 0.8 0.08 0.5
+a = 0.7
+b = 0.8
+epsilon = 0.08
+I = 0.5
+
+#definicja ukladu rownan
+def fhn(t, y):
+    v, w = y
+    dvdt = v - (v**3)/3 - w + I
+    dwdt = epsilon * (v + a - b*w)
+    return [dvdt, dwdt]
+
+#przedział czasu i punkty czasowe
+t_span = (0, 200)
+t_eval = np.linspace(t_span[0], t_span[1], 5000)
+
+#warunki poczatkowe
+y0 = [-1.0, 1.0]
+
+#rozwiazanie numeryczne
+solution = solve_ivp(fhn, t_span, y0, t_eval=t_eval)
+
+#wykres przebiegu czasowego potencjalu
+plt.figure(figsize=(8, 4))
+plt.plot(solution.t, solution.y[0])
+plt.xlabel("czas t")
+plt.ylabel("potencjał v(t)")
+plt.title("przebieg czasowy potencjału neuronu – model fitzhugh–nagumo")
+plt.grid(True)
+plt.show()
+
+#portret fazowy
+plt.figure(figsize=(5, 5))
+plt.plot(solution.y[0], solution.y[1])
+plt.xlabel("v")
+plt.ylabel("w")
+plt.title("portret fazowy modelu fitzhugh–nagumo")
+plt.grid(True)
+plt.show()
